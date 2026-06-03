d19cc0b048642818c8cf38a60ffdf02ef0e4209f
dattran2k4 <tranquocthanhdat@gmail.com>
2026-06-03 16:34:32 +0700

    upd: add log info in webhook controller

--- backend/src/main/java/com/mentormatching/modules/payment/presentation/StripeWebhookController.java
+++ backend/src/main/java/com/mentormatching/modules/payment/presentation/StripeWebhookController.java
@@ -1,6 +1,7 @@
 package com.mentormatching.modules.payment.presentation;
 
 import com.mentormatching.modules.payment.infrastructure.stripe.StripeWebhookHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/payments/stripe/webhook")
 public class StripeWebhookController {
 
@@ -23,6 +25,7 @@ public class StripeWebhookController {
     @PostMapping
     public ResponseEntity<Void> handleStripeWebhook(@RequestBody String payload,
                                                     @RequestHeader(name = STRIPE_SIGNATURE_HEADER, required = false) String signature) {
+        log.info("Stripe webhook called");
         stripeWebhookHandler.handle(payload, signature);
         return ResponseEntity.ok().build();
     }
